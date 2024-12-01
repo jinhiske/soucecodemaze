8f1ddeb100caced9dd6b988a8460228159a4e8fe
Jung jincheol <jjk3522@gmail.com>
2024-12-01 20:11:22 +0900

    랜덤으로 미로가 바뀌도록 변경

--- sw_mazegame/sw_mazegame_20212220/mazegm.c
+++ sw_mazegame/sw_mazegame_20212220/mazegm.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <conio.h>
 
-#define WIDTH 10
-#define HEIGHT 10
+#define WIDTH 21
+#define HEIGHT 11
 
 // 미로: 0은 길, 1은 벽, 2는 출구
-int maze[HEIGHT][WIDTH] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 1, 0, 1, 1, 0, 1},
-    {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
-    {1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    {1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    {1, 0, 1, 1, 1, 1, 1, 1, 2, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
+int maze[HEIGHT][WIDTH];
 
 // 플레이어 위치
 int playerX = 1, playerY = 1;
+
+// 방향 이동 배열 (상, 하, 좌, 우)
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
 
 // 화면 출력
 void drawMaze() {
@@ -44,6 +38,43 @@ void drawMaze() {
     }
 }
 
+// 미로 초기화
+void initializeMaze() {
+    // 모든 칸을 벽(1)으로 채움
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            maze[y][x] = 1;
+        }
+    }
+}
+
+// 미로 생성 (DFS 기반)
+void generateMaze(int x, int y) {
+    maze[y][x] = 0; // 현재 위치를 길로 설정
+
+    // 방향을 랜덤하게 섞음
+    int directions[] = { 0, 1, 2, 3 };
+    for (int i = 0; i < 4; i++) {
+        int r = rand() % 4;
+        int temp = directions[i];
+        directions[i] = directions[r];
+        directions[r] = temp;
+    }
+
+    // 섞은 방향으로 이동
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[directions[i]] * 2;
+        int ny = y + dy[directions[i]] * 2;
+
+        // 이동할 위치가 유효하고, 벽인 경우
+        if (nx > 0 && ny > 0 && nx < WIDTH - 1 && ny < HEIGHT - 1 && maze[ny][nx] == 1) {
+            // 중간 벽도 제거
+            maze[y + dy[directions[i]]][x + dx[directions[i]]] = 0;
+            generateMaze(nx, ny);
+        }
+    }
+}
+
 // 입력 처리
 void movePlayer(char input) {
     int nextX = playerX, nextY = playerY;
@@ -61,7 +92,16 @@ void movePlayer(char input) {
 }
 
 int main() {
-    printf("미로 탈출 게임에 오신 것을 환영합니다!\n");
+    srand(time(NULL));
+
+    // 미로 초기화 및 생성
+    initializeMaze();
+    generateMaze(1, 1);
+
+    // 출구 설정
+    maze[HEIGHT - 2][WIDTH - 2] = 2;
+
+    printf("랜덤 미로 탈출 게임에 오신 것을 환영합니다!\n");
     printf("플레이어는 'P'로 표시됩니다. 출구는 'E'입니다.\n");
     printf("방향키(WASD)를 사용하여 이동하세요.\n");
     printf("엔터 키를 누르면 게임이 시작됩니다!\n");
