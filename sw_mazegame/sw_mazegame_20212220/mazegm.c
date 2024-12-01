4d9417f14ead595f63b7a6bc6a7473285fc9c6a2
Jung jincheol <jjk3522@gmail.com>
2024-12-01 20:41:10 +0900

    스테이지를 만듬

--- sw_mazegame/sw_mazegame_20212220/mazegm.c
+++ sw_mazegame/sw_mazegame_20212220/mazegm.c
@@ -3,14 +3,18 @@
 #include <time.h>
 #include <conio.h>
 
-#define WIDTH 21
-#define HEIGHT 11
+// 초기 미로 크기 및 단계 설정
+#define INITIAL_WIDTH 11
+#define INITIAL_HEIGHT 11
+#define MAX_LEVEL 3
 
-// 미로: 0은 길, 1은 벽, 2는 출구
-int maze[HEIGHT][WIDTH];
+// 미로 배열 크기 제한
+#define MAX_SIZE 51
 
-// 플레이어 위치
-int playerX = 1, playerY = 1;
+int maze[MAX_SIZE][MAX_SIZE]; // 미로 배열
+int width, height;           // 현재 미로 크기
+int playerX, playerY;        // 플레이어 위치
+int level = 1;               // 현재 레벨
 
 // 방향 이동 배열 (상, 하, 좌, 우)
 int dx[] = { 0, 0, -1, 1 };
@@ -19,8 +23,8 @@ int dy[] = { -1, 1, 0, 0 };
 // 화면 출력
 void drawMaze() {
     system("cls");
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
             if (x == playerX && y == playerY) {
                 printf("P "); // 플레이어
             }
@@ -40,10 +44,9 @@ void drawMaze() {
 
 // 미로 초기화
 void initializeMaze() {
-    // 모든 칸을 벽(1)으로 채움
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            maze[y][x] = 1;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            maze[y][x] = 1; // 모든 칸을 벽으로 초기화
         }
     }
 }
@@ -67,7 +70,7 @@ void generateMaze(int x, int y) {
         int ny = y + dy[directions[i]] * 2;
 
         // 이동할 위치가 유효하고, 벽인 경우
-        if (nx > 0 && ny > 0 && nx < WIDTH - 1 && ny < HEIGHT - 1 && maze[ny][nx] == 1) {
+        if (nx > 0 && ny > 0 && nx < width - 1 && ny < height - 1 && maze[ny][nx] == 1) {
             // 중간 벽도 제거
             maze[y + dy[directions[i]]][x + dx[directions[i]]] = 0;
             generateMaze(nx, ny);
@@ -91,15 +94,29 @@ void movePlayer(char input) {
     }
 }
 
-int main() {
-    srand(time(NULL));
+// 단계 시작
+void startLevel() {
+    printf("Level %d 시작!\n", level);
+    getchar();
 
     // 미로 초기화 및 생성
     initializeMaze();
     generateMaze(1, 1);
 
     // 출구 설정
-    maze[HEIGHT - 2][WIDTH - 2] = 2;
+    maze[height - 2][width - 2] = 2;
+
+    // 플레이어 초기 위치
+    playerX = 1;
+    playerY = 1;
+}
+
+int main() {
+    srand(time(NULL));
+
+    // 초기 미로 크기 설정
+    width = INITIAL_WIDTH;
+    height = INITIAL_HEIGHT;
 
     printf("랜덤 미로 탈출 게임에 오신 것을 환영합니다!\n");
     printf("플레이어는 'P'로 표시됩니다. 출구는 'E'입니다.\n");
@@ -107,19 +124,32 @@ int main() {
     printf("엔터 키를 누르면 게임이 시작됩니다!\n");
     getchar();
 
-    while (1) {
-        drawMaze();
+    while (level <= MAX_LEVEL) {
+        startLevel();
 
-        // 출구 도달 체크
-        if (maze[playerY][playerX] == 2) {
-            printf("축하합니다! 미로를 탈출했습니다!\n");
-            break;
+        // 현재 단계 진행
+        while (1) {
+            drawMaze();
+
+            // 출구 도달 체크
+            if (maze[playerY][playerX] == 3) {
+                printf("축하합니다! Level %d을(를) 클리어했습니다!\n", level);
+                break;
+            }
+
+            // 입력 대기 및 처리
+            char input = _getch();
+            movePlayer(input);
         }
 
-        // 입력 대기 및 처리
-        char input = _getch();
-        movePlayer(input);
+        // 다음 단계로 진행
+        level++;
+        if (level <= MAX_LEVEL) {
+            width += 10;   // 미로 너비 증가
+            height += 10;  // 미로 높이 증가
+        }
     }
 
+    printf("축하합니다! 모든 단계를 클리어했습니다!\n");
     return 0;
 }
